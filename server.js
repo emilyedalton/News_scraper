820eca413c1601991bb961067c0769916521cce1
Emily Dalton <emilydalton@Emilys-Air.lan>
2019-02-23 16:03:00 -0800

    handlebars working initial routes laid out

--- server.js
+++ server.js
@@ -1,6 +1,58 @@
-const express = require("express");
-mongoose = require("mongoose");
-handlebars = require("handlebars");
-cheerio = require("cheerio");
-axios = require ("axios")
 
+// var express = require('express');
+// var exphbs  = require('express-handlebars');
+
+// var app = express();
+
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
+
+// app.listen(3000);
+
+
+
+
+const express = require("express");
+ mongoose = require("mongoose");
+ cheerio = require("cheerio");
+ axios = require ("axios")
+ app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// body parsing middleware
+
+app.use(express.static("public"));
+
+// Parse request body as JSON
+// app.use(express.urlencoded({ extended: true })); 
+// app.use(express.json());
+
+// set view engine
+const exphbs = require("express-handlebars");
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get("/",(req,res)=>{
+res.render('index');
+
+});
+
+app.post("/new",(req, res)=>{
+
+
+
+
+});
+
+app.get("/delete/:id", (req,res)=>{
+res.send("this will delete one by ID")
+});
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});  

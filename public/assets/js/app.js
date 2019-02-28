e8a2f2a2a1d2aab8b5cc25a3743b2190c1a26c7e
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-02-28 09:48:27 -0600

    button firing

---
+++ public/assets/js/app.js
@@ -0,0 +1,16 @@
+
+// $(document).ready(function() {
+
+// $(".waves-effect waves-light btn-large").on("click",  function() {
+$(document).on("click", ".btn-large", function(){   
+// $("#button").on("click",  function() {
+
+console.log("I'm working");
+    // $("#newsDiv").empty();
+    // $.getJSON("/scrape", function (data) {
+
+
+
+    // });
+});
+// });

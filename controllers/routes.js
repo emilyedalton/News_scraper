6698b71ef8383486b942060789720bf5fabf6dcb
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2019-03-01 19:10:41 -0600

    comments almost working

--- controllers/routes.js
+++ controllers/routes.js
@@ -39,30 +39,51 @@ app.get("/scrape", function (req, res) {
     });
 });
 
-app.get("/articles", (req, res) => {
-    db.Article.find({}, function (err, allArticles) {
-        // if(err){
-        //   console.log(err)
-        // }else{
-        // console.log("these are articles" + " " +allArticles);
-        res.render('index', { articles: allArticles });
+app.get("/", (req, res) => {
+    //populate("notes").sort({_id:-1 }).exec
+  
+    //Emily, populate is a method to show the detail of the child
+    //note in articles collections. In index.handlebars, I reference
+    //note.body to display the comments.
+    db.Article.find({}).populate("note").exec(
+      function (err, allArticles) {
+        if (err) {
+            res.status(500).end(err);
+        } else {
+            res.render('index', { articles: allArticles });
         console.log("article result")
+        }
 
-        // console.log(allArticles);
-        // }
-
-    }).sort({ _id: -1 }).populate("notes");
-
+    });
+  
+    //THIS How
+    // .sort({ _id: -1 });
+  
+       
+  
 });
 
+// app.get("/articles", (req, res) => {
+//     db.Article.find({}, function (err, allArticles) {
+//         // if(err){
+//         //   console.log(err)
+//         // }else{
+//         // console.log("these are articles" + " " +allArticles);
+//         res.render('index', { articles: allArticles });
+//         console.log("article result")
+
+//         // console.log(allArticles);
+//         // }
+
+//     }).sort({ _id: -1 }).populate("notes");
+
+// });
+
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function (req, res) {
-    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+app.get("/articles/:id", (req, res) => {
     db.Article.findOne({ _id: req.params.id })
-        // ..and populate all of the notes associated with it
         .populate("notes")
         .then(function (dbArticle) {
-            // If we were able to successfully find an Article with the given id, send it back to the client
             res.json(dbArticle);
         }).catch(err => {
             res.status(500).end(err);

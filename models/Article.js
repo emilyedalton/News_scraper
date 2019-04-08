f77e2c1a178e50c5f2d4b17c0061987e6d8bb0b0
Emily Dalton <emilydalton@dhcp-10-105-33-155.wireless.northwestern.private>
2019-04-08 13:03:01 -0500

    adding correct route to button

--- models/Article.js
+++ models/Article.js
@@ -6,16 +6,13 @@ var Schema = mongoose.Schema;
 var ArticleSchema = new Schema({
   title: {
     type: String,
-    required: true
   },
   link: {
     type: String,
-    required: true
   },
 
   subject: {
     type: String,
-    required:true
   },
 
   saved: {
